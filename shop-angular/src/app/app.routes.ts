18b144b3195908029376d235c841acf1561f79c8
Sol <haninz7393@gmail.com>
2025-02-28 00:43:27 +0200

    Re-add backend and frontend as normal folders

---
+++ shop-angular/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    {
+        path: 'Home',
+        component: HomeComponent
+    },
+    //add a component for page not found
+];
